285dbca7d92c5f5ba2bbdbb5395c11ab054d0d72
DXBLT24\Thasidaran <thasidaran@gtechme.com>
2019-05-29 12:37:26 +0400

    Updates

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function() {
     //DOM Loaded Event
 
     app.init();
-    //app.firebase();
+    app.firebase();
 
 
 });
@@ -27,16 +27,35 @@ const app = {
 
     installServiceWorkerAsync();
 
-    function updateOnlineStatus() {
-        document.getElementById('status').innerText = 'Online';
-      }
-      
-      function updateOfflineStatus() {
-        document.getElementById('status').innerText = 'Offline';
-      }
-      
-      window.addEventListener('online',  updateOnlineStatus);
-      window.addEventListener('offline', updateOfflineStatus);
+    let deferredPrompt;
+    const btnAdd = document.getElementById('install');
+
+    window.addEventListener('beforeinstallprompt', (e) => {
+        // Prevent Chrome 67 and earlier from automatically showing the prompt
+        e.preventDefault();
+        // Stash the event so it can be triggered later.
+        deferredPrompt = e;
+
+        // Update UI notify the user they can add to home screen
+        btnAdd.style.display = 'block';
+    });
+
+    btnAdd.addEventListener('click', (e) => {
+        // hide our user interface that shows our A2HS button
+        btnAdd.style.display = 'none';
+        // Show the prompt
+        deferredPrompt.prompt();
+        // Wait for the user to respond to the prompt
+        deferredPrompt.userChoice
+          .then((choiceResult) => {
+            if (choiceResult.outcome === 'accepted') {
+              console.log('User accepted the A2HS prompt');
+            } else {
+              console.log('User dismissed the A2HS prompt');
+            }
+            deferredPrompt = null;
+          });
+      });
 
     },
 
@@ -58,14 +77,32 @@ const app = {
         // Retrieve Firebase Messaging object.
         const messaging = firebase.messaging();
 
-
         messaging.requestPermission()
         .then(function(){
            console.log("Have Permission");
            return messaging.getToken();
         })
         .then(function(token){
-            console.log(token);
+            
+            messaging.getToken().then(function(currentToken) {
+                if (currentToken) {
+                  //sendTokenToServer(currentToken);
+                  //updateUIForPushEnabled(currentToken);
+
+                  console.log(currentToken);
+                } else {
+                  // Show permission request.
+                  console.log('No Instance ID token available. Request permission to generate one.');
+                  // Show permission UI.
+                  //updateUIForPushPermissionRequired();
+                  //setTokenSentToServer(false);
+                }
+              }).catch(function(err) {
+                console.log('An error occurred while retrieving token. ', err);
+                //showToken('Error retrieving Instance ID token. ', err);
+                //setTokenSentToServer(false);
+              });
+
         })
         .catch(function(err){
             console.log("Error") 
